0c153d8837476afa078658024e98fed3aa9c58e5
Irina <irina2358@yandex.ru>
2021-06-25 19:17:58 +0300

    Добавлено DZ1

---
+++ project/script.js
@@ -0,0 +1,48 @@
+const itemsCatalog = [
+    { id: 0, name: 'Mango People T-shirt', description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: '$52.00' },
+    { id: 1, name: 'Mango People T-shirt', description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: '$52.00' },
+    { id: 2, name: 'Mango People T-shirt', description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: '$52.00' },
+    { id: 3, name: 'Mango People T-shirt', description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: '$52.00' },
+    { id: 4, name: 'Mango People T-shirt', description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: '$52.00' },
+    { id: 5, name: 'Mango People T-shirt', description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: '$52.00' },
+
+];
+
+
+const renderFeaturedItem = (item) => {
+    return `
+    <div class="featuredItem">
+
+        <div class="featuredImgWrap">
+            <img src="images/featured/${item.id}.jpg" alt="">
+                <div class="featuredImgDark">
+                    <button data-productId="images/cart.png">
+                        <img src="images/cart.svg" alt="">
+                            Add to Cart
+                    </button>
+                </div>
+                </div>
+
+                <div class="featuredData">
+                    <div class="featuredName">
+                        ${item.name}
+                    </div>
+                    <div class="featuredText">
+                        ${item.description}
+                    </div>
+                    <div class="featuredPrice">
+                        ${item.price}
+                    </div>
+                </div>
+
+        </div>
+    `;
+};
+
+const renderFeaturedList = (list = itemsCatalog) => {
+    let itemsCatalog = list.map(item => renderFeaturedItem(item));
+    document.querySelector('.featuredItems').innerHTML = itemsCatalog.join('');
+}
+
+renderFeaturedList(itemsCatalog);
+
